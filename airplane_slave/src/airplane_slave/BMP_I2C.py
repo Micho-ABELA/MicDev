1f5a1d4a27eae0d2c129a97b6c84bbaf774d52b2
Micho-ABELA <67414072+Micho-ABELA@users.noreply.github.com>
2022-07-05 17:27:37 +0200

    Add files via upload

--- airplane_slave/src/airplane_slave/BMP_I2C.py
+++ airplane_slave/src/airplane_slave/BMP_I2C.py
@@ -77,8 +77,8 @@ class Device(object):
         self._address = address
         if i2c_interface is None:
             # Use pure python I2C interface if none is specified.
-            import Adafruit_PureIO.smbus
-            self._bus = Adafruit_PureIO.smbus.SMBus(busnum)
+            import BMP_PureIO
+            self._bus = BMP_PureIO.SMBus(busnum)
         else:
             # Otherwise use the provided class to create an smbus interface.
             self._bus = i2c_interface(busnum)
