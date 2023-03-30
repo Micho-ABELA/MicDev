757365795fc679b7ac518d0bbad26df114aec8cb
Micho-ABELA <67414072+Micho-ABELA@users.noreply.github.com>
2023-03-30 16:25:40 +0200

    Delete hosting_website directory

--- hosting_website/get_file_names.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dir = $_GET['dir'];
-    $files = array_diff(scandir($dir), array('..', '.'));
-
-    header('Content-type: application/json');
-    echo json_encode(array('files' => $files));
-?>
